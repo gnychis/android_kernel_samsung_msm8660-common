5abdac39fe95aa721acd026a3dc38162a052fbaa
George Nychis <gnychis@gmail.com>
2012-08-28 13:30:34 -0400

    kernel is now successfully being built, including packages etc

---
+++ noclean_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
+cd ../compat-wireless-2012-05-10/
+make clean
+./scripts/driver-select rt2x00
+KERNEL_DIR=$(pwd)/../sgs2-cm9-kernel
+make ARCH=arm CROSS_COMPILE=arm-eabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@"
+cd ../sgs2-cm9-kernel
+./package.sh
