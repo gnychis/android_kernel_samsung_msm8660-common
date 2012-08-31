cc61cfbd851344f0c1dc94fdc0a16cb9494b2f4e
George Nychis <gnychis@gmail.com>
2012-08-31 16:54:23 -0400

    attempting to put modules in a different place for installation

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 rm -f initramfs/lib/modules/*
 rm -f package_tree/system/lib/modules/*
 rm -f package_tree/boot.img
-find . -name "*.ko" -exec cp {} initramfs/system/lib/modules \;
+find . -name "*.ko" -exec cp {} package_tree/system/lib/modules \;
 #mv initramfs/system/lib/modules/mac80211.ko /tmp/
 #mv initramfs/system/lib/modules/cfg80211.ko /tmp/
 ./mkbootfs initramfs | gzip > ramdisk.gz
@@ -11,8 +11,8 @@ cp arch/arm/boot/zImage .
 cp boot.img package_tree
 rm zImage
 #find ../compat-wireless-3.0.9-1/ -name "*.ko" -exec cp {} package_tree/system/lib/modules/ \;
-mv /tmp/mac80211.ko package_tree/system/lib/modules/
-mv /tmp/cfg80211.ko package_tree/system/lib/modules/
+#mv /tmp/mac80211.ko package_tree/system/lib/modules/
+#mv /tmp/cfg80211.ko package_tree/system/lib/modules/
 cd package_tree
 zip -r ../sgs2_cm9_awmon.zip *
 cd ../
