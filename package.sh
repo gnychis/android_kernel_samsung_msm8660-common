2e69ad3574cb047370b41caf66fc2c83a145dc8b
George Nychis <gnychis@gmail.com>
2012-09-02 16:47:37 -0400

    trying to add proper firmware in

--- package.sh
+++ package.sh
@@ -8,7 +8,7 @@ cp arch/arm/boot/zImage .
 ./mkbootimg-sg2x --kernel zImage --ramdisk ramdisk.gz --cmdline "androidboot.hardware=qcom usb_id_pin_rework=true" -o boot.img --base 0x40400000 --pagesize 2048
 cp boot.img package_tree
 rm zImage
-find ../compat-wireless-3.0.9-1/ -name "*.ko" -exec cp {} package_tree/system/lib/modules/ \;
+find ../compat-wireless-3.5.1-1/ -name "*.ko" -exec cp {} package_tree/system/lib/modules/ \;
 cd package_tree
 zip -r ../sgs2_cm9_awmon.zip *
 cd ../
