10ddf85843a52165c7762086a4ec0c25878f950d
George Nychis <gnychis@gmail.com>
2012-10-06 12:33:22 -0700

    adding in the ae1000 firmware for the linksys dongle

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-make ARCH=arm clean
-make ARCH=arm cyanogenmod_awmon_defconfig
-make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
-#cd ../compat-wireless-3.0.9-1/
-#make clean
-#./scripts/driver-select rt2x00
-#KERNEL_DIR=$(pwd)/../sgs2-cm9-kernel
-#make ARCH=arm CROSS_COMPILE=arm-eabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@"
-#cd ../sgs2-cm9-kernel
+export ARCH=arm
+export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
+make clean
+make clean
+make cyanogenmod_awmon_defconfig
+make -j2
 ./package.sh
--- noclean_build.sh
+++ noclean_build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
-#cd ../compat-wireless-3.0.9-1/
-#make clean
-#./scripts/driver-select restore
-#./scripts/driver-select rt2x00
-#KERNEL_DIR=$(pwd)/../sgs2-cm9-kernel
-#make ARCH=arm CROSS_COMPILE=arm-eabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@"
-#cd ../sgs2-cm9-kernel
+export ARCH=arm
+export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
+make -j2
 ./package.sh
