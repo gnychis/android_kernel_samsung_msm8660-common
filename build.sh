8712825611be939929714db77e3209043b765809
George Nychis <gnychis@gmail.com>
2012-10-06 12:45:02 -0700

    adding in the different toolchains to build from

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export ARCH=arm
-export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6`/bin/arm-eabi-
+export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
 make clean
 make clean
 make cyanogenmod_awmon_defconfig
-make -j2
+make
 ./package.sh
