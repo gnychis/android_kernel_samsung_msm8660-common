47913e43e67ae98519c93e08ca5bd1cc7531b77f
George Nychis <gnychis@gmail.com>
2012-10-06 13:43:16 -0700

    more toolchains

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ export ARCH=arm
 #export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6`/bin/arm-linux-androideabi-
 #export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6`/bin/arm-eabi-
 export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
+#export CROSS_COMPILE=`readlink -f ../android-ndk-r9b-linux/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86`/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=`readlink -f ../android-ndk-r7b-linux/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86`/bin/arm-linux-androideabi-
 make clean
 make clean
 make cyanogenmod_awmon_defconfig
